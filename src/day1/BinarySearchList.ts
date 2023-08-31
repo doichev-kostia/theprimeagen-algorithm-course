0ec51c6998a977307bdf6100ccc838c9392edd57
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-08-31 23:16:11 +0200

    feat: bsl

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+	let low = 0;
+	let high = haystack.length;
 
+	do {
+		const diff = (high - low) / 2
+		const middle = Math.floor(low + diff);
+
+		const value = haystack[middle];
+
+		if (value === needle) {
+			return true;
+		} else if (value < needle) {
+			low = middle + 1;
+		} else {
+			high = middle;
+		}
+
+	} while( low < high );
+
+	return false;
 }
