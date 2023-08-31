c831a09ff0ff801c4cd9b04208fb3ea2acb53bca
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-08-31 23:58:09 +0200

    feat: bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,14 @@
 export default function bubble_sort(arr: number[]): void {
-
+	// [1, 7, 3, 4]
+	for (let i = 0; i < arr.length; i += 1) {
+		for (let j = 0; j < arr.length - 1 - i; j++) {
+			const left = arr[j];
+			const right = arr[j + 1]
+			if (left > right) {
+				const tmp = left;
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
 }
