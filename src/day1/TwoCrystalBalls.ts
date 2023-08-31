9d48028bd577387400727359c893768a819e3ec7
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-08-31 23:17:22 +0200

    feat: two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,28 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+	let breakpoint = Math.floor(Math.sqrt(breaks.length));
 
+	for (let i = 0; i < breaks.length; i += breakpoint) {
+		if (i + breakpoint > breaks.length) {
+			const diff = breaks.length - i;
+			breakpoint = diff; // so I do not have an overflow
+		}
+
+		const result = breaks[i];
+		if (!result) {
+			continue;
+		}
+
+		let prev = 0;
+		if (i - breakpoint >= 0) { // to prevent an overflow
+			prev = i - breakpoint;
+		}
+
+		for (let j = prev; j < i; j+= 1) {
+			if (breaks[j]) {
+				return j;
+			}
+		}
+	}
+
+	return -1;
 }
