dc4304e191770f464f13cfc6e12d3748af51c16a
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-09-20 08:38:55 +0200

    feat: queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,51 @@
+class Node<T> {
+	constructor(public data: T, public next: Node<T> | null) {
+	}
+}
+
 export default class Queue<T> {
-    public length: number;
+	public length: number;
+	private head: Node<T> | null;
+	private tail: Node<T> | null;
 
-    
+	constructor() {
+		this.head = this.tail = null;
+		this.length = 0;
+	}
 
-    constructor() {
-    }
+	enqueue(item: T): void {
+        const node = new Node(item, null);
+        this.length += 1;
 
-    enqueue(item: T): void {
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
 
-}
-    deque(): T | undefined {
+        this.tail.next = node;
+        this.tail = node;
+	}
 
-}
-    peek(): T | undefined {
+	deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length -= 1;
+
+        const node = this.head;
+        this.head = this.head.next;
+
+        node.next = null; // GC
+
+        if (this.length === 0) {
+            this.tail = null;
+        }
+
+        return node.data;
+	}
+
+	peek(): T | undefined {
+        return this.head?.data;
+	}
 }
