d60fe1d6fbdf5bb3225ebdf3dd04cdd2f22a4ca3
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-09-21 07:20:36 +0200

    feat: stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,48 @@
+
+// (a) <- (b) <- (c)
+//                ^ head
+class Node<T> {
+	constructor(public data: T, public prev: Node<T> | null) {
+	}
+}
+
+
 export default class Stack<T> {
-    public length: number;
+	public length: number;
+	private head: Node<T> | null;
 
-    
+	constructor() {
+		this.length = 0;
+		this.head = null;
+	}
 
-    constructor() {
-    }
+	push(item: T): void {
+		this.length += 1;
+		const node = new Node(item, this.head);
 
-    push(item: T): void {
+		if (this.head) {
+			node.prev = this.head;
+		}
 
-}
-    pop(): T | undefined {
+		this.head = node;
+	}
 
-}
-    peek(): T | undefined {
+	pop(): T | undefined {
+		const node = this.head;
 
-}
+		if (!node) {
+			return undefined;
+		}
+		this.length -= 1;
+
+		this.head = this.head?.prev ?? null;
+
+		node.prev = null; // GC
+
+		return node.data;
+	}
+
+	peek(): T | undefined {
+		return this.head?.data;
+	}
 }
