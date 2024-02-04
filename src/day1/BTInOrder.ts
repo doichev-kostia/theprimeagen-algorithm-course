077b4e2599705848871934200abf413cf806f003
Kostiantyn Doichev <doichev.kostia@gmail.com>
2024-02-04 15:15:30 +0100

    in order walk

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,26 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number>, path: number[]): void {
+	if (!node.left) {
+		path.push(node.value);
+	}
 
+	if (node.left == null && node.right == null) {
+		return;
+	}
+
+	if (node.left) {
+		walk(node.left, path);
+		path.push(node.value)
+	}
+
+	if (node.right) {
+		walk(node.right, path);
+	}
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+	const path: number[] = [];
+
+	walk(head, path);
+
+	return path;
 }
