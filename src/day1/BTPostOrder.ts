65f3dd940404936db2b371abe755dec2a558479f
Kostiantyn Doichev <doichev.kostia@gmail.com>
2024-02-04 15:17:37 +0100

    post order walk

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,24 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number>, path: number[]): void {
+	if (node.left == null && node.right == null) {
+		path.push(node.value)
+		return;
+	}
 
+	if (node.left) {
+		walk(node.left, path);
+	}
+
+	if (node.right) {
+		walk(node.right, path);
+	}
+
+	path.push(node.value)
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+	const path: number[] = [];
+
+	walk(head, path);
+
+	return path;
 }
