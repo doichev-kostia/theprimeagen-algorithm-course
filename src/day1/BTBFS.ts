0b4850864184cb8ad6cc5b3fa68f6dc934b40129
Kostiantyn Doichev <doichev.kostia@gmail.com>
2024-02-04 16:08:45 +0100

    breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,31 @@
+import Queue from "@code/Queue";
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+	const queue = new Queue<BinaryNode<number>>();
+
+	queue.enqueue(head)
+
+	let found = false
+	while (queue.length) {
+		const node = queue.deque() as BinaryNode<number>;
+		if (node.value === needle) {
+			found = true;
+			break;
+		}
+
+		if (node.left == null && node.right == null) {
+			continue;
+		}
+
+		if (node.left) {
+			queue.enqueue(node.left);
+		}
+
+		if (node.right) {
+			queue.enqueue(node.right);
+		}
+	}
+
+	return found;
 
 }
