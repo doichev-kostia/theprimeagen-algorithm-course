5187acd0ca6ac41130080cde28ddd516e541decf
Kostiantyn Doichev <doichev.kostia@gmail.com>
2024-02-04 18:36:59 +0100

    binary search

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,23 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
 
+function search(node: BinaryNode<number>, needle: number): boolean {
+	// not really needed, but come on, we are in JS land. as any is the king :)
+	if (!node) {
+		return false
+	}
+	if (node.value === needle) {
+		return true
+	}
+
+	if (node.value < needle) {
+		return !!node.right && search(node.right, needle);
+	}
+
+	return !!node.left && search(node.left, needle);
+}
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+	if (!head) {
+		return false;
+	}
+
+	return search(head, needle);
 }
