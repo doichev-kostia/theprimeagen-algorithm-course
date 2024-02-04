30f705fb059fbde94ecf92c361f64a85359e07a3
Kostiantyn Doichev <doichev.kostia@gmail.com>
2024-02-04 14:55:40 +0100

    immutable walk

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -14,10 +14,30 @@ function walk(node: BinaryNode<number>, path: number[]): void {
 	}
 }
 
+function walk_immutable(node: BinaryNode<number>, path: number[]): number[] {
+	let p = [...path, node.value]
+
+	if (node.left == null && node.right == null) {
+		return p
+	}
+
+	if (node.left) {
+		p = walk_immutable(node.left, p);
+	}
+
+	if (node.right) {
+		p = walk_immutable(node.right, p)
+	}
+
+	return p;
+}
+
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-	const path: number[] = [];
+	// const path: number[] = [];
+	//
+	// walk(head, path);
+	//
+	// return path;
 
-	walk(head, path);
-
-	return path;
+	return walk_immutable(head, []);
 }
